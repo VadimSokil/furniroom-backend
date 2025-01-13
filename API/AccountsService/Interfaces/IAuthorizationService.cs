a74f1c5190aef62da35f5f8e5fe18541a57a7396
DESKTOP-SVLI0O8\User <sokol3389@gmail.com>
2025-01-13 10:50:14 +0200

    interface fix

--- API/AccountsService/Interfaces/IAuthorizationService.cs
+++ API/AccountsService/Interfaces/IAuthorizationService.cs
@@ -1,10 +1,11 @@
-﻿using AccountsService.Models.Authorization;
+﻿using AccountsService.Models;
+using AccountsService.Models.Authorization;
 
 namespace AccountsService.Interfaces
 {
     public interface IAuthorizationService
     {
-        public Task<string> CheckEmailAsync(string email);
+        public Task<ResponseModel> CheckEmailAsync(string email);
         public Task<string> GenerateCodeAsync(string email);
         public Task<string> ResetPasswordAsync(string email);
         public Task<string> RegisterAsync(RegisterModel register);
