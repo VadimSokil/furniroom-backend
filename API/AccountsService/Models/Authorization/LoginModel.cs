3f63bc5cd9e2f54c49421e3afce2afbe479777af
DESKTOP-SVLI0O8\User <sokol3389@gmail.com>
2025-01-17 23:28:36 +0200

    modifided response system in controller 8

--- API/AccountsService/Models/Authorization/LoginModel.cs
+++ API/AccountsService/Models/Authorization/LoginModel.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace AccountsService.Models.Authorization
+﻿namespace AccountsService.Models.Authorization
 {
     public class LoginModel
     {
-        [Required]
         public string? Email { get; set; }
-        [Required]
         public string? PasswordHash { get; set; }
     }
 }
