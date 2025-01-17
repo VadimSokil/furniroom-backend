995191816c56d7e61dcefdf599c18b8432fb441f
DESKTOP-SVLI0O8\User <sokol3389@gmail.com>
2025-01-17 22:35:15 +0200

    modifided response system in controller 5

--- API/AccountsService/Controllers/AuthorizationController.cs
+++ API/AccountsService/Controllers/AuthorizationController.cs
@@ -3,6 +3,7 @@ using AccountsService.Interfaces;
 using AccountsService.Models.Authorization;
 using AccountsService.Models.Response;
 using AccountsService.Validation;
+using System.Text.Json;
 
 namespace AccountsService.Controllers
 {
@@ -287,17 +288,10 @@ namespace AccountsService.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<ResponseModel>> Login([FromBody] LoginModel login)
+        public async Task<ActionResult<ResponseModel>> Login([FromBody] JsonElement loginJson)
         {
-            if (!ModelState.IsValid)
-            {
-                return new ResponseModel
-                {
-                    Date = currentDateTime,
-                    RequestExecution = false,
-                    Message = "The structure of the data is incorrect"
-                };
-            }
+            var login = loginJson.Deserialize<LoginModel>();
+
             if (login == null)
             {
                 return new ResponseModel
@@ -307,6 +301,31 @@ namespace AccountsService.Controllers
                     Message = "Invalid data structure. Please check your input."
                 };
             }
+            else if (!ModelState.IsValid)
+            {
+                return new ResponseModel
+                {
+                    Date = currentDateTime,
+                    RequestExecution = false,
+                    Message = "The structure of the data is incorrect"
+                };
+            }
+            var jsonKeys = loginJson.EnumerateObject().Select(kv => kv.Name).ToHashSet();
+            var modelProperties = typeof(LoginModel).GetProperties().Select(p => p.Name).ToHashSet();
+
+            foreach (var key in jsonKeys)
+            {
+                if (!modelProperties.Contains(key))
+                {
+                    return new ResponseModel
+                    {
+                        Date = currentDateTime,
+                        RequestExecution = false,
+                        Message = "Invalid data structure. Extra field detected: " + key
+                    };
+                }
+            }
+
             if (!validationMethods.IsString(login.Email))
             {
                 return new ResponseModel
