20a20c4341a64d0646cda39fb9aa5e70d9664b67
DESKTOP-SVLI0O8\User <sokol3389@gmail.com>
2025-01-20 19:25:04 +0200

    service fix 4

--- API/FurniroomAPI/Services/AccountService.cs
+++ API/FurniroomAPI/Services/AccountService.cs
@@ -45,13 +45,13 @@ namespace FurniroomAPI.Services
 
         public async Task<ServiceResponseModel> GetAccountInformationAsync(int accountId)
         {
-            var endpoint = $"{_endpointURL["GetAccountInformation"]}?accountId={accountId}";
+            var endpoint = _endpointURL["GetAccountOrders"] + "?accountId=" + accountId;
             return await GetDataAsync(endpoint);
         }
 
         public async Task<ServiceResponseModel> GetAccountOrdersAsync(int accountId)
         {
-            var endpoint = $"{_endpointURL["GetAccountOrders"]}?accountId={accountId}";
+            var endpoint = _endpointURL["GetAccountOrders"] + "?accountId=" + accountId;           
             return await GetDataAsync(endpoint);
         }
 
